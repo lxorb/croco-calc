4e9538c05ed469c05482cf710ac9512b7cc62a4d
lxorb <101118850+lxorb@users.noreply.github.com>
2026-08-02 09:28:37 +0200

    rename monkeytype to croco calc

--- packages/oxlint-config/plugins/croco-calc-rules.js
+++ packages/oxlint-config/plugins/croco-calc-rules.js
@@ -66,7 +66,7 @@ function containsJSXReturn(node) {
 
 const plugin = {
   meta: {
-    name: "monkeytype-rules",
+    name: "croco-calc-rules",
   },
   rules: {
     "no-testing-access": defineRule({
